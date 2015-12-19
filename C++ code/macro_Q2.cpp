7d0ff5762e67269212031b7eee8eaa860d7dccc5
Brad Paynter <bpaynter@uco.edu>
2015-12-18 23:24:50 -0600

    Reorganized and added C++ NetBeans project

---
+++ C++ code/macro_Q2.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   macro_Q2.cpp
+ * Author: bpaynter
+ *
+ * Created on December 18, 2015, 4:28 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
