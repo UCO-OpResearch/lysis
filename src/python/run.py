b51dfb1a969dfb6458dbd5c083161244184949ad
bpaynter <bpaynter@uco.edu>
2023-01-16 10:29:28 -0600

    Added pre-'restricted move' data from Buddy. Added new py-macro run data from buddy.

--- src/python/run.py
+++ src/python/run.py
@@ -1,6 +1,8 @@
 import cProfile
 import os
 
+from datetime import datetime
+
 import lysis
 
 
@@ -9,6 +11,17 @@ p = {'total_time': 200}
 e.initialize_macro_param(p)
 macro = lysis.MacroscaleRun(e)
 os.makedirs(os.path.join(e.os_path, "macro_pstats"), exist_ok=True)
+now = datetime.now()
+timestamp = str.join('', [str(now.year),
+                          '_',
+                          str(now.month),
+                          '_',
+                          str(now.day),
+                          '_',
+                          str(now.hour),
+                          str(now.minute),
+                          str(now.second)
+                          ])
 last_stats_num = 0
 filename = None
 while True:
