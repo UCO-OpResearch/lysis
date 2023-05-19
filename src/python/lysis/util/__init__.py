4143c52c0f88f6807006579616612f2d8c9467a2
Brad Paynter <bpaynter@uco.edu>
2023-05-19 07:35:35 -0500

    Fixed animation rendering for new degradation output. Added code for 'Model Schematic' diagram.

--- src/python/lysis/util/__init__.py
+++ src/python/lysis/util/__init__.py
@@ -5,3 +5,10 @@ from .parameters import *
 from .util import *
 from .codeutil import *
 from .edge_grid import *
+
+try:
+    import matplotlib.pyplot
+    from .curlyBrace import *
+except ImportError:
+    print("Matplotlib import error")
+    pass
