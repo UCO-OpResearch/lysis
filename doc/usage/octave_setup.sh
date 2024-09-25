857e54a9aafbb7da15a6645df915ccf37797ffb8
Brad Paynter <bpaynter@uco.edu>
2024-09-25 08:45:37 -0500

    Adding octave setup script

---
+++ doc/usage/octave_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load Anaconda3
+conda init
+source ~/.bashrc
+
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda create -y --name lysis python=3.11
+conda activate lysis
+conda install -y black jupyterlab jupyter-black matplotlib pandas octave-kernel
