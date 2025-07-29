4c10c1e4e67dc7eb1674efbef605ba859eed9a21
Brad Paynter <bpaynter@uco.edu>
2025-07-29 13:12:35 -0500

    Added datastore code to read HDF5 and export "micro to macro" data.

--- src/python/lysis/util/datastore.py
+++ src/python/lysis/util/datastore.py
@@ -1,3 +1,5 @@
+import os
+
 from enum import Flag, auto, unique
 from typing import Any, AnyStr, List, Mapping, Union
 
@@ -44,6 +46,40 @@ class DataStatus(Flag):
     INITIALIZED = auto()
     LOADED = auto()
     SAVED = auto()
+    FILLED = auto()
+
+
+def h5_tree(val: h5py.Dataset, pre: AnyStr = ""):
+    """Recursively prints the tree of an HDF5 file's contents.
+
+    Copied from https://stackoverflow.com/questions/61133916/is-there-in-python-a-single-function-that-shows-the-full-structure-of-a-hdf5-fi
+
+    Args:
+        val: The item in the HDF5 to print
+        pre: The current indentation
+    """
+    items = len(val)
+    for key, val in val.items():
+        items -= 1
+        if items == 0:
+            # the last item
+            if type(val) == h5py._hl.group.Group:
+                print(pre + "└── " + key)
+                h5_tree(val, pre + "    ")
+            else:
+                try:
+                    print(pre + "└── " + key + " (%d)" % len(val))
+                except TypeError:
+                    print(pre + "└── " + key + " (scalar)")
+        else:
+            if type(val) == h5py._hl.group.Group:
+                print(pre + "├── " + key)
+                h5_tree(val, pre + "│   ")
+            else:
+                try:
+                    print(pre + "├── " + key + " (%d)" % len(val))
+                except TypeError:
+                    print(pre + "├── " + key + " (scalar)")
 
 
 class DataStore:
@@ -51,21 +87,91 @@ class DataStore:
     A simple data store class that allows for storing and retrieving data.
     """
 
-    def __init__(self, run=None):
+    _internal_names: List[str] = [
+        "internal_names",
+        "run_code",
+        "path",
+        "data",
+        "tables",
+        "views",
+        "status",
+        "mode",
+    ]
+
+    def __init__(self, run_code: AnyStr, path: AnyStr, mode: str = "r"):
         """
         Initialize the DataStore with an optional run parameter.
-        :param run: The experimental run associated with the data store.
-        """
-        self.run_code = run
-        self.status = DataStatus.NONE
 
-    def import_fortran_micro_data(self, filecode=None):
+        :param run: The experimental run associated with the data store.
+        :param mode: Mode in which to open the file.
+
+            ``'r'``
+                Read only
+
+            ``'w'``
+                Write (NOTE: This will overwrite any current file contents)
+
+            ``'a'``
+                Append. Will allow the addition of new data only.
+                Will NOT allow the insertion of data if some already exists.
+        """
+        object.__setattr__(self, "_run_code", run_code)
+        object.__setattr__(self, "_path", path)
+        object.__setattr__(self, "_mode", mode)
+        object.__setattr__(
+            self,
+            "_status",
+            {
+                "self": DataStatus.INITIALIZED,
+                "micro": DataStatus.NONE,
+                "macro": DataStatus.NONE,
+            },
+        )
+
+        object.__setattr__(self, "_tables", [])
+        object.__setattr__(self, "_views", [])
+
+        # Initialize HDF5 file
+        h5py.get_config().track_order = True
+        object.__setattr__(
+            self,
+            "_data",
+            h5py.File(
+                os.path.join(self._path, f"{self._run_code}.h5"),
+                self._mode,
+            ),
+        )
+
+        if "micro_data" in self._data:
+            self._status["micro"] = DataStatus.INITIALIZED
+        else:
+            self._status["micro"] = DataStatus.NONE
+
+        if "macro_data" in self._data:
+            self._status["macro"] = DataStatus.INITIALIZED
+        else:
+            self._status["macro"] = DataStatus.NONE
+
+        # TODO: Add code here to check if there is actually data stored in this HDF5
+        #       and if it matches the current data specification
+
+    def __str__(self):
+        """Print the datastore in human-readable format."""
+        return h5_tree(self._data)
+
+    def import_fortran_micro_data(
+        self, filecode: AnyStr = None, data_version: AnyStr = "current"
+    ):
         """
         Import data from a Fortran Microscale run into the HDF5 storage.
 
         :param filecode: The file code associated with the Microscale run being imported.
         """
-        pass
+        if self._mode == "r":
+            raise os.UnsupportedOperation("Data is open in read-only mode.")
+        if self._mode == "a" and self._status["micro"] == DataStatus.INITIALIZED:
+            raise os.UnsupportedOperation("Existing data cannot be overwritten.")
+        micro_data = self._data.create_group("micro_data")
 
     def import_fortran_macro_data(self, filecode=None):
         """
@@ -79,9 +185,57 @@ class DataStore:
         """
         Export Microscale data from the HDF5 storage to disk for use by a Fortran Macroscale run.
 
+        See the "Macro to Micro files" section of the Data Specification for more information.
+
         :param filecode: The file code associated with the Microscale run being exported.
         """
-        pass
+        # TODO: Add code to check if microscale data exists
+        # Get the microscale data
+        micro_data = self._data["micro_data"]
+        # Get the number of microscale runs and set the dimensions of the bins so that we get 100 bins
+        bin_size = micro_data["pli_first_time"].size // 100
+        # tPAleave is the CDF of the tPA leaving time distribution.
+        # This is really just a list of edgepoints from the bins for tPA leaving time
+        # These bins are evenly distributed along the interval [0, 1]
+        tPAleave = np.append(np.arange(0, 1, 0.01), [1.0])
+        np.savetxt(os.path.join(self._path, f"tPAleave_{filecode}.dat"), tPAleave)
+
+        # The remaining data will be arranged into 100 bins
+        # according to the time tPA left the simulation.
+        # Get the sorted ordering of the tPA leaving times
+        indices = micro_data["tpa_leaving_time"][:].argsort()
+        # Find the tPA leaving times for the edges of each bin.
+        tsectPA = np.append(
+            [0], micro_data["tpa_leaving_time"][:][indices[bin_size - 1 :: bin_size]]
+        )
+        np.savetxt(os.path.join(self._path, f"tsectPA_{filecode}.dat"), tsectPA)
+
+        # Identify which simulations had the fiber fully degraded
+        lysis_complete = micro_data["fiber_degraded"][:]
+        # Read in the fiber degradation times for all simulations
+        lysis_time = micro_data["sim_final_time"][:]
+        # If full degradation did NOT occur,
+        # this matrix currently contains the ending time of the simulation.
+        # Replace these times with an 'infinity' marker of 6,000 seconds
+        lysis_time[~lysis_complete] = 6_000
+        # Rearrange the matrix so that each row contains the lysis times for simulations
+        # corresponding to the matching bin in the ``tsectPA`` vector.
+        # Then sort the rows (bins) individually by lysis time.
+        # Finally, transpose the matrix so that the bins are arranged in columns.
+        lysismat = np.stack(
+            [
+                np.sort(lysis_time[indices[i * bin_size : (i + 1) * bin_size]])
+                for i in range(100)
+            ]
+        ).T
+        np.savetxt(os.path.join(self._path, f"lysismat_{filecode}.dat"), lysismat)
+
+        # Find the location of the first '6000' entry in each column of the ``lysismat`` matrix
+        # Then convert to 1-indexing.
+        lenlysisvect = lysismat.argmax(axis=1) + 1
+        np.savetxt(
+            os.path.join(self._path, f"lenlysisvect_{filecode}.dat"), lenlysisvect
+        )
 
     def __getattr__(self, key: AnyStr) -> np.ndarray:
         """
